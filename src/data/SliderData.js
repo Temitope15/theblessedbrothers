290a0672348be11406b48b48fc317cf643955c4d
Sunmade <temitopeakinsunmade08@gmail.com>
2026-01-18 09:25:10 +0100

    Update image URLs in SliderData for improved visual content

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -4,35 +4,35 @@
 
 export const sliderData = [
   {
-    imageUrl: "https://ibb.co/MY9FK61",
+    imageUrl: "https://i.ibb.co/8LdFctsB/a1babdbe-5f51-421f-86c4-1d797deef430.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/39SBp5t0",
+    imageUrl: "https://i.ibb.co/1f2GwVR9/21798a73-bdb5-4cd3-ae8d-b516b8bbad8a.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/9HsB7N4z",
+    imageUrl: "https://i.ibb.co/Xf7SZwQB/c81cde13-bfd8-42da-972b-3564a10959b4.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/Pv9BQ0jY",
+    imageUrl: "https://i.ibb.co/5hBCP6jt/bdcd96e0-bbde-4d84-9b12-a9d850d11adb.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/39SBp5t0",
+    imageUrl: "https://i.ibb.co/B2pph6K0/fa33024e-02a8-4039-8d2d-5eaa4ca48797.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/Pv9BQ0jY",
+    imageUrl: "https://i.ibb.co/qtxTh5g/6dab0371-a858-49b0-9a67-e0d6fead918a.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/Pv9BQ0jY",
+    imageUrl: "https://i.ibb.co/mrGVFYcY/5a0fcafc-192a-485f-a6ba-95123b56182e.jpg",
     details: "2025 End of the year general meeting",
   },
   {
-    imageUrl: "https://ibb.co/Pv9BQ0jY",
+    imageUrl: "https://i.ibb.co/q3mkNLbR/f1294f12-7c02-468c-a3b7-f7d0215473fd.jpg",
     details: "2025 End of the year general meeting",
   },
   {
